bf4b2a02b42beaa61c0a440dbd58a52be7e8827c
Darkedoviz <146472779+Darkedoviz@users.noreply.github.com>
2025-05-13 19:47:26 +0500

    Write headmenu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-    <auth/>
+    <hmenu/>
 </template>
 
 <script>
 import auth from './components/auth.vue';
+import hmenu from './components/hmenu.vue';
+
 export default {
 
     components: { 
-        auth
+        hmenu
     },
     data() {
         return {}; 
---
+++ src/components/hmenu.vue
@@ -0,0 +1,98 @@
+<template>
+    <header class="header">
+        <div class="container header-container">
+            <div class="header-left">
+                <button class="menu-toggle" @click="toggleMenu" aria-label="Toggle menu"
+                    :class="{ 'white-icon': isMenuOpen }">
+                    <i :class="menuIcon"></i>
+                </button>
+
+                <div class="logo" :class="{ 'white-icon': isMenuOpen }">
+                    <i class="ri-wallet-3-line"></i>
+                    <span>loGMa</span>
+                </div>
+            </div>
+
+            <div class="header-right">
+                <button class="theme-toggle" @click="toggleTheme" aria-label="Toggle theme">
+                    <i :class="themeIcon"></i>
+                </button>
+
+                <router-link v-if="isAuth === 1" to="/profile" class="user-profile">
+                    <img v-if="userAvatar" :src="userAvatar" alt="User avatar" class="avatar" />
+                    <i v-else class="ri-user-line"></i>
+                </router-link>
+                <router-link v-else to="/auth" class="auth-btn">
+                    <i class="ri-login-box-line"></i>
+                </router-link>
+            </div>
+
+            <nav class="nav" :class="{ show: isMenuOpen }">
+                <router-link to="/welcome" class="nav-link">
+                    <i class="ri-home-4-line"></i> Главная
+                </router-link>
+                <router-link to="/history" class="nav-link">
+                    <i class="ri-history-line"></i> История
+                </router-link>
+                <router-link to="/analytics" class="nav-link">
+                    <i class="ri-pie-chart-2-line"></i> Аналитика
+                </router-link>
+
+                <details class="nav-details">
+                    <summary class="nav-summary">
+                        <i class="ri-group-line"></i> Группы
+                    </summary>
+                    <div class="nav-dropdown">
+                        <router-link v-for="group in groups" to="/groups" class="nav-link dropdown-link">
+                            {{ group.name }}
+                        </router-link>
+                    </div>
+                </details>
+
+                <router-link to="/categories" class="nav-link">
+                    <i class="ri-price-tag-3-line"></i> Категории
+                </router-link>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            isMenuOpen: false,
+            isDarkTheme: false,
+            isAuth: 0,
+            userAvatar: '',
+            groups: [
+                { name: "Семья" },
+                { name: "Друзья" },
+                { name: "Коллеги" }
+            ]
+        }
+    },
+    computed: {
+        themeIcon() {
+            return this.isDarkTheme ? 'ri-sun-line' : 'ri-moon-line'
+        },
+        menuIcon() {
+            return this.isMenuOpen ? 'ri-close-line' : 'ri-menu-line'
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen
+        },
+        toggleTheme() {
+            this.isDarkTheme = !this.isDarkTheme
+        },
+        handleAddExpense() {
+            this.$emit('add-expense')
+        }
+    }
+}
+</script>
+
+<style src="/css/header.css"></style>
